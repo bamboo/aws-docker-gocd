ab6de56785d440619fc4a1ab3f21c1ce0156839e
Rodrigo B. de Oliveira <rbo@acm.org>
2014-06-06 08:48:41 -0300

    pack the app

--- app/gulpfile.js
+++ app/gulpfile.js
@@ -3,18 +3,32 @@ require('source-map-support').install();
 var gulp = require('gulp');
 var mjs = require('gulp-mjs');
 var es = require('event-stream');
+var zip = require('gulp-zip');
 
 var paths = {
   src: ['index.mjs'],
-  dest: '.'
+  dest: '.',
+  zip: {
+    src: [
+      'Dockerfile', 'package.json', '*.js',
+      'node_modules/express/**',
+      'node_modules/source-map-support/**'],
+    name: 'app.zip',
+    dest: '.'}
 };
 
-gulp.task('build', function() {
+gulp.task('build', function () {
   return combine(
     gulp.src(paths.src),
     mjs({debug: true}),
-    gulp.dest(paths.dest))
-  .on('error', onError);
+    gulp.dest(paths.dest));
+});
+
+gulp.task('pack', function () {
+  return combine(
+    gulp.src(paths.zip.src, {cwdbase: true}),
+    zip(paths.zip.name),
+    gulp.dest(paths.zip.dest));
 });
 
 function onError(err) {
@@ -22,7 +36,7 @@ function onError(err) {
 }
 
 function combine() {
-  return es.pipeline.apply(null, arguments);
+  return es.pipeline.apply(null, arguments).on('error', onError);
 }
 
 gulp.task('default', ['build']);
